81b4d3e3468ffabc3f830a8fb25f5bca12e3af63
BearmanCodes <idostuff1225@gmail.com>
2022-08-24 21:37:23 -0400

    did some stuff with player controller

--- It starts with/Assets/PlayerController.cs
+++ It starts with/Assets/PlayerController.cs
@@ -1,31 +1,33 @@
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
-    public float jumpForce;
+    public Rigidbody2D poopy;
+    public float speed = 5.2f;
     public float moveInput;
-    public Rigidbody2D rb;
+    public float jumpForce;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        poopy = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow)){
-            rb.velocity = Vector2.up * jumpForce;
+            poopy.velocity = Vector2.up * speed;
         }
     }
 
     void FixedUpdate()
     {
         moveInput = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        poopy.velocity = new Vector2(moveInput * speed, poopy.velocity.y);
     }
+
 }
